72464ae97dd56cdd28035f243a820d9da0cd2fe4
nnminh31 <67349480+nnminh31@users.noreply.github.com>
2020-11-05 20:59:20 +0700

    fix header

--- BTTH/demo.php
+++ BTTH/demo.php
@@ -33,7 +33,7 @@
                                 <img src="Images/en.jpg" alt="" class="lgg">
                             </div>
                         </div>
-                        
+                        <br><br>
                         <div class="form-inline search d-none d-md-block" style="float:right;">
                             <div class="form-group">
                                 <span class="glyphicon glyphicon-search"></span>
@@ -164,27 +164,29 @@
         </div>
     </nav>
     <menu style="background-color: #0A1F77; margin: 0 0 0 0;">
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="Images/baner-tuyen.jpg" class="d-block w-100" alt="...">
+        <div class="container">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="Images/baner-tuyen.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="Images/banner-webthumb.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="Images/nganh-thuy-loi-vietthumb.jpg" class="d-block w-100" alt="...">
+                    </div>
                 </div>
-                <div class="carousel-item">
-                    <img src="Images/banner-webthumb.jpg" class="d-block w-100" alt="...">
+                <div>  
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
-                <div class="carousel-item">
-                    <img src="Images/nganh-thuy-loi-vietthumb.jpg" class="d-block w-100" alt="...">
-                </div>
-            </div>
-            <div>  
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
             </div>
         </div>
     </menu>
